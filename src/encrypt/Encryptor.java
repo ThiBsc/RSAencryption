bceb9cafc7ebed671ca015cd8e0d6b094bbd46ae
Thibaut B <thibDev@users.noreply.github.com>
2018-02-17 16:08:59 +0100

    Improve encrypt performance

--- src/encrypt/Encryptor.java
+++ src/encrypt/Encryptor.java
@@ -9,9 +9,11 @@ public class Encryptor {
 	
 	public static Vector<BigInteger> encrypt(String message, PublicKey pubk){
 		Vector<BigInteger> crypted = new Vector<BigInteger>();
+		BigInteger e = pubk.getPublicPair().e;
+		BigInteger n = pubk.getPublicPair().n;
 		for (int c=0; c<message.length(); c++){
 			BigInteger bi = new BigInteger(Integer.toString(message.charAt(c)));
-			crypted.add( bi.modPow(pubk.getPublicPair().e, pubk.getPublicPair().n) );
+			crypted.add( bi.modPow(e, n) );
 		}
 		return crypted;
 	}
